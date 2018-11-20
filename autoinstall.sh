ff45e3306cecca55ac8c6133e7b4c3d2b10af431
hshi <snapwings3190@163.com>
2018-11-20 12:46:03 +0800

    sources.list

--- autoinstall.sh
+++ autoinstall.sh
@@ -8,3 +8,12 @@ cd $dir_path/remote-check
 cp remote-cp-compute.desktop $dir_path/
 cp check-net.desktop $dir_path/
 echo "桌面会有check-net和remote-cp-compute的图标，依次点击这两个图标！"
+
+
+sudo /etc/apt/sources.list /etc/apt/sources.list.old
+cd $dir_path
+sudo cp -p /sources-list/163.sources.list /etc/apt/sources.list
+sudo chown root:root /etc/apt/sources.list
+sudo chmod 664 /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -f expect
